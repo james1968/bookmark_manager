a79ec7b86c83fc75009a59601eec56d6cfd1d20a
Liam Taylor <liam.taylor888@googlemail.com>
2016-10-17 16:33:05 +0100

    Feature test for link route passing

--- spec/features/links_spec.rb
+++ spec/features/links_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 require './app/models/link'
+Capybara.app = BookmarkManager
 
 feature 'view links' do
   scenario 'display list of links' do
-    Link.create(
-    :url => "www.bbc.co.uk/news" ,
-    :title => "Interesting webpage" )
-    visit ('/')
+    Link.create(:url => "www.bbc.co.uk/news", :title => "Interesting webpage")
+    visit ('/links')
     expect(page.status_code).to eq 200
     within 'ul#links' do
       expect(page).to have_content "Interesting webpage"
